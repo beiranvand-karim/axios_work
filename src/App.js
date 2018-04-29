08bc5d38992dfcda6299756d6f78708eaa8b8621
beiranvand.karim <beiranvand.karim@gmail.com>
2018-04-29 12:33:27 +0430

    refresh problom fix up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
 import './App.scss';
 import Home from './containers/Home';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-
 
 class App extends Component {
   render() {
     return (
-        <Router>
-            <Switch>
-                <Route path="/" exact component={Home} />
-            </Switch>
-        </Router>
+        <Home />
     );
   }
 }

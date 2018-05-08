dbcba0b6fd46ff5232b9f9c748242588661b374a
beiranvand.karim <beiranvand.karim@gmail.com>
2018-05-08 14:36:45 +0430

    nav bar styling

--- src/components/home/Navigation.js
+++ src/components/home/Navigation.js
@@ -2,99 +2,102 @@ import React from 'react';
 import './Navigation.scss';
 import whiteBlockchain from '../../assets/img/white-blockchain.svg';
 
-export default function Navigation() {
+export default class Navigation extends React.Component {
 
-    return (
+    render() {
 
-        <nav className="navigation">
+        return (
 
-            <ul className="igation">
+            <nav id="navigation">
 
-                <li className="first">
+                <ul className="igation">
 
-                    <a href="">
-                        <img src={whiteBlockchain} alt=""/>
-                    </a>
-                    <button>
+                    <li className="first">
+
+                        <a href="">
+                            <img src={whiteBlockchain} alt=""/>
+                        </a>
+                        <button>
                         <span>
 
                         </span>
-                    </button>
-                    <button>
+                        </button>
+                        <button>
 
-                    </button>
-                </li>
+                        </button>
+                    </li>
 
-                <li className="with-children">
-                    <a href="">Wallet</a>
-                    <ul>
-                        <li>
-                            <a href="">Login</a>
-                        </li>
-                    </ul>
-                </li>
-                <li className="with-children">
+                    <li className="with-children Wallet">
+                        <a href="">Wallet</a>
+                        <ul>
+                            <li>
+                                <a href="">Login</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li className="with-children DATA">
 
-                    <a href="">DATA</a>
-                    <ul>
-                        <li>
-                            <a href="">Charts</a>
-                        </li>
-                        <li>
-                            <a href="">Stats</a>
-                        </li>
-                        <li>
-                            <a href="">Markets</a>
-                        </li>
-                    </ul>
-                </li>
-                <li className="with-children">
+                        <a href="">DATA</a>
+                        <ul>
+                            <li>
+                                <a href="">Charts</a>
+                            </li>
+                            <li>
+                                <a href="">Stats</a>
+                            </li>
+                            <li>
+                                <a href="">Markets</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li className="with-children API">
 
-                    <a href="">API</a>
-                    <ul>
-                        <li>
-                            <a href="">Business</a>
-                        </li>
-                    </ul>
-                </li>
-                <li className="with-children">
-                    <a href="">About</a>
-                    <ul>
+                        <a href="">API</a>
+                        <ul>
+                            <li>
+                                <a href="">Business</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li className="with-children about">
+                        <a href="">About</a>
+                        <ul>
 
-                        <li>
-                            <a href="">Team</a>
-                        </li>
+                            <li>
+                                <a href="">Team</a>
+                            </li>
 
-                        <li>
-                            <a href="">Careers</a>
-                        </li>
+                            <li>
+                                <a href="">Careers</a>
+                            </li>
 
-                        <li>
-                            <a href="">Press</a>
-                        </li>
+                            <li>
+                                <a href="">Press</a>
+                            </li>
 
-                        <li>
-                            <a href="">Blog</a>
-                        </li>
-                    </ul>
-                </li>
+                            <li>
+                                <a href="">Blog</a>
+                            </li>
+                        </ul>
+                    </li>
 
-                <li className="flex-space">
+                    <li className="flex-space">
 
-                </li>
+                    </li>
 
-                <li>
-                    <form action="">
-                        <input className="search-bar" placeholder="block, hash, transaction, etc..." name="search" type="text"/>
-                    </form>
-                </li>
+                    <li className="pre-last">
+                        <form action="">
+                            <input className="search-bar" placeholder="block, hash, transaction, etc..." name="search" type="text"/>
+                        </form>
+                    </li>
 
-                <li className="last">
-                    <a className="wallet-button" href="" target="_blank">Get A Free Wallet</a>
-                </li>
+                    <li className="last">
+                        <a className="wallet-button" href="" target="_blank">Get A Free Wallet</a>
+                    </li>
 
-            </ul>
-        </nav>
-    )
+                </ul>
+            </nav>
+        )
+    }
 
 }
